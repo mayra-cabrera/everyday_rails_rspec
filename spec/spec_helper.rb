b506f0524dfd718e0f419dd5b196476f956d2255
Mayra Cabrera <mcabrera1087@gmail.com>
2014-06-30 20:50:14 -0500

    Share examples across multiple describe and context blockes. Also reduce more reptition with helper macros, and creates a custom Rspec matchers for require_login

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,16 +13,6 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -41,4 +31,6 @@ RSpec.configure do |config|
   config.order = "random"
   # Include FactoryGirl syntax to simplify  calls to factories
   config.include FactoryGirl::Syntax::Methods
+
+  config.include LoginMacros
 end
